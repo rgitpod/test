f37240861d0db75815c2a9f8d4dc3e48e782dd1c
Dio Eraclea <gauss120792@gmail.com>
2022-11-29 21:16:21 +0000

    Update 3 files

--- components/Home.js
+++ components/Home.js
@@ -1,12 +1,18 @@
 import { StyleSheet, Text, View, Button } from "react-native";
 import MenuButton from "./MenuButton";
 
-export default function Home({ navigation }) {
+export default function Home() {
   return (
     <>
-      <MenuButton title="Фонетика" navTo="Phonetics" navigation={navigation} />
-      <Button title="Морфология" onPress={() => navigation.navigate("Morphemics")} />      
-      <Button title="Орфография" onPress={() => navigation.navigate("Spelling")} />
+      <MenuButton title="Фонетика" navTo="Phonetics" />
+      <Button
+        title="Морфология"
+        onPress={() => navigation.navigate("Morphemics")}
+      />
+      <Button
+        title="Орфография"
+        onPress={() => navigation.navigate("Spelling")}
+      />
     </>
   );
-} 
+}
--- components/Note.js
+++ components/Note.js
@@ -1,14 +1,12 @@
-import { StyleSheet, Text, View } from 'react-native';
-import { list } from './contents/data/List'
+import { StyleSheet, Text, View } from "react-native";
+import { list } from "./contents/data/List";
 
 export default function Note({ navigation, route }) {
-  const name = route.params.name
-  const id = route.params.id
-  
+  const name = route.params.name;
+  const id = route.params.id;
   return (
     <View>
       <Text>{list[name][id].text}</Text>
     </View>
   );
 }
-
