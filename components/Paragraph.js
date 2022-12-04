2d5bf560316eb5a5233ff114cc7f70ac599fb3b0
Dio Eraclea <gauss120792@gmail.com>
2022-12-04 01:13:47 +0000

    Update 9 files

--- components/Paragraph.js
+++ components/Paragraph.js
@@ -1,7 +1,7 @@
 import { StyleSheet, Text, View } from "react-native";
 import { index } from "./contents/data/index";
 
-export default function Note({ navigation, route }) {
+export default function Paragraph({ navigation, route }) {
   const name = route.params.name;
   const id = route.params.id;
   console.log(route.params.name)
